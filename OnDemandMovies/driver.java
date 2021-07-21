046e6445a447c1e1ef477a5c1444f1756e2eaa8f
Blane Staskiewicz <bas6248@psu.edu>
2021-07-21 19:06:04 -0400

    Update driver.java

--- OnDemandMovies/driver.java
+++ OnDemandMovies/driver.java
@@ -1,16 +1,22 @@
 package movies;
 import java.util.*;
 
+/**
+* <h1>Movie Database</h1>
+*
+* @author  Blane Staskiewicz
+*/
 public class driver{
 	/** This method is the main/driver method
-	 * 
-	 * @throws Exception 
-	 * @call   calls other methods to validate and output movies
-	 */
+	* 
+	* @throws Exception 
+	* @call   calls valid method to validate and output movies
+	* @param  args  Unused
+	*/
 	public static void main(String[] args) throws Exception{
 		ArrayList<OnDemandMovie> database = new ArrayList<OnDemandMovie>();
 		
-		//movie list
+		// movie list
 		OnDemandMovie obj0 = new OnDemandMovie("Reservoir Dogs", "R", "Drama/Mystery", "Quentin Tarantino", "Harvey Keitel", 3.99, "03/18/20", 7);
 		database.add(obj0);
 		OnDemandMovie obj1 = new OnDemandMovie("Pulp Fiction", "R", "Drama/Crime", "Quentin Tarantino", "John Travolta", 5.95, "05/23/20", 4);
@@ -32,56 +38,57 @@ public class driver{
 		OnDemandMovie obj9 = new OnDemandMovie("Once Upon a Time in Hollywood", "R", "Drama/Comedy-drama", "Quentin Tarantino", "Leonardo DiCaprio", 15.99, "07/09/21", 2);
 		database.add(obj9);
 		
-    	Scanner s = new Scanner(System.in);
+    		Scanner s = new Scanner(System.in);
         
 		System.out.println("Movies available to rent: ");
 		System.out.println();
         
-		//prints movies
-    	for(int i = 0; i < database.size(); i++){
-    		int location = i + 1;
-    		System.out.println("Choice " + location + ": ");
-    		System.out.println(database.get(i).toString());
-    		System.out.println();
-    	}
+		// prints movies
+		for(int i = 0; i < database.size(); i++){
+			int location = i + 1;
+			System.out.println("Choice " + location + ": ");
+			System.out.println(database.get(i).toString());
+			System.out.println();
+		}
 
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println();
-        
-        //get price based on input
-        double payment = 0;
-        int user_input = valid(s);
-        for(int i = 0; i < database.size(); i++){
-        	if((i + 1) == user_input){
-        		payment = database.get(i).getPrice();
-        	}
-        }
-        
-        //end output after choosing a movie to rent
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println();
+
+		// get price based on input
+		double payment = 0;
+		int user_input = valid(s);
+		for(int i = 0; i < database.size(); i++){
+			if((i + 1) == user_input){
+				payment = database.get(i).getPrice();
+			}
+		}
+
+		// end output after choosing a movie to rent
 		System.out.print("Rental Payment due: ");
 		System.out.println(payment);
 		System.out.println("Thank you for your business!");
 	}
-	/** This method is used to make sure input value is a valid number
-	 * 
-	 * @param	s
-	 */
+	/** 
+	* This method is used to make sure input value is a valid number
+	* 
+	* @param s  Scanner used to get user input
+	*/
 	public static int valid(Scanner s){
 		int user_input = 0;
 		while(user_input != 1 && user_input != 2 && user_input != 3 && user_input != 4 && user_input != 5 && user_input != 6
 				&& user_input != 7 && user_input != 8 && user_input != 9 && user_input != 10) {
 			try{
-	    		System.out.print("Please enter the number of the movie you wish to rent: ");
-	    		String input2 = s.nextLine();
-	    		int user_input1 = Integer.parseInt(input2);
-	    		System.out.println();
-	    		
-	    		//user input is not between 1 - 10
-	    		if(user_input1 != 1 && user_input1 != 2 && user_input1 != 3 && user_input1 != 4 && user_input1 != 5 && user_input1 != 6
+				System.out.print("Please enter the number of the movie you wish to rent: ");
+				String input2 = s.nextLine();
+				int user_input1 = Integer.parseInt(input2);
+				System.out.println();
+
+				// user input is not between 1 - 10
+				if(user_input1 != 1 && user_input1 != 2 && user_input1 != 3 && user_input1 != 4 && user_input1 != 5 && user_input1 != 6
 	    				&& user_input1 != 7 && user_input1 != 8 && user_input1 != 9 && user_input1 != 10) {
-	    			throw new InputMismatchException();
-	    		}
-	    		return user_input1;
+	    				throw new InputMismatchException();
+	    			}
+	    			return user_input1;
 			}
 			catch(Exception e){
 				System.out.println("Exception: java.util.InputMismatchException");
@@ -90,6 +97,6 @@ public class driver{
 				System.out.println();
 			}
 		}
-		return 0; //not supposed to hit this
+		return 0; // not supposed return here
 	}
 }
